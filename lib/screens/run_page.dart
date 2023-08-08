d71b6cff57e171de86c51b333a0aec2c72af403a
NomuKen13 <110008733+NomuKen13@users.noreply.github.com>
2023-08-08 16:15:21 +0900

    Revert "Feature/add shake page"

--- lib/screens/run_page.dart
+++ lib/screens/run_page.dart
@@ -5,81 +5,53 @@ import 'package:beatim5/widgets/music_controll_button.dart';
 import 'package:beatim5/widgets/page_transition_button.dart';
 import 'package:flutter/material.dart';
 
-class RunPage extends StatefulWidget {
-  final double playbackBpm;
+class RunPage extends BaseLayout {
+  @override
+  String get title => 'Running !';
+  String get explanation => 'Start running!\nGrip your smartphone and start swinging your arm\nMusic automatically starts.';
 
-  RunPage({required this.playbackBpm});
 
   @override
-  _RunPageState createState() => _RunPageState();
-}
-
-class _RunPageState extends State<RunPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Run Page'),
-      ),
-      body: Center(
-        child: Text(
-          'BPM: ${widget.playbackBpm}',
-          style: const TextStyle(fontSize: 24),
-        ),
+  Widget mainContent(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(70.0,20.0,70.0,0.0),
+            child: Image.asset('images/logo.png'),
+          ),
+          MusicControllButton(),
+        ],
       ),
     );
   }
+
+  @override
+  Widget buttomContent(BuildContext context) {
+    return Column(
+      children: [
+        PageTransitionButton(
+            'Finish Running',
+          Navigator.push<void>(
+            context,
+            MaterialPageRoute<void>(
+              builder: (BuildContext context) => FinishRunPage(),
+            ),
+          ),
+        ),
+        SizedBox(height: 10,),
+        PageTransitionButton(
+            'Switch walking',
+            () {
+              Navigator.push<void>(
+                context,
+                MaterialPageRoute<void>(
+                  builder: (BuildContext context) => ChoosePlaylistPage(),
+                ),
+              );
+            }
+        )
+      ],
+    );
+  }
 }
-
-
-// class RunPage extends BaseLayout {
-//
-//   @override
-//   String get title => 'Running !';
-//   @override
-//   String get explanation => 'Start running!\nGrip your smartphone and start swinging your arm\nMusic automatically starts.';
-//
-//   @override
-//   Widget mainContent(BuildContext context) {
-//     return Center(
-//       child: Column(
-//         children: [
-//           Padding(
-//             padding: const EdgeInsets.fromLTRB(70.0,20.0,70.0,0.0),
-//             child: Image.asset('images/logo.png'),
-//           ),
-//           const MusicControllButton(),
-//         ],
-//       ),
-//     );
-//   }
-//
-//   @override
-//   Widget buttomContent(BuildContext context) {
-//     return Column(
-//       children: [
-//         PageTransitionButton(
-//             'Finish Running',
-//           Navigator.push<void>(
-//             context,
-//             MaterialPageRoute<void>(
-//               builder: (BuildContext context) => FinishRunPage(),
-//             ),
-//           ),
-//         ),
-//         const SizedBox(height: 10,),
-//         PageTransitionButton(
-//             'Switch walking',
-//             () {
-//               Navigator.push<void>(
-//                 context,
-//                 MaterialPageRoute<void>(
-//                   builder: (BuildContext context) => ChoosePlaylistPage(),
-//                 ),
-//               );
-//             }
-//         )
-//       ],
-//     );
-//   }
-// }
