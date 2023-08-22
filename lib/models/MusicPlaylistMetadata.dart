a38b71ad887e76ace0c5305c82ac6d49a2f45200
NomuKen13 <110008733+NomuKen13@users.noreply.github.com>
2023-08-22 16:48:14 +0900

    add MusicPlaylistMetadata class

---
+++ lib/models/MusicPlaylistMetadata.dart
@@ -0,0 +1,11 @@
+class MusicPlaylistMetadata {
+
+  MusicPlaylistMetadata(this.title,this.subTitle,this.music1);
+
+  final String title;
+  final String subTitle;
+  final String music1;
+
+}
+
+List<MusicPlaylistMetadata> MusicPlaylistMetadataCollection =[];
