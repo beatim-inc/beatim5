f2ef064d96791b655f12f861469a47b7f2d68954
marufura <tara20070827@gmail.com>
2023-09-19 15:18:49 +0900

    add movePacePage

--- lib/screens/run_page.dart
+++ lib/screens/run_page.dart
@@ -7,8 +7,10 @@ import 'package:beatim5/models/MusicMetadata.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';
 
+import '../functions/get_goal_speed.dart';
 import '../functions/get_or_generate_user_id.dart';
 
 class RunPage extends StatefulWidget {
@@ -22,13 +24,18 @@ class RunPage extends StatefulWidget {
 
 class _RunPageState extends State<RunPage> {
   double playbackBpm;
-  AudioPlayer player = AudioPlayer();
-
-  speedMeterLogManager? speedMeterLog;
 
   _RunPageState({required this.playbackBpm});
 
-  double goalSpeed = 1.7;
+  AudioPlayer player = AudioPlayer();
+  speedMeterLogManager? speedMeterLog;
+
+  double? goalSpeed;
+
+  _initializeGoalSpeed() async {
+    goalSpeed = await getGoalSpeed();
+    setState(() {});
+  }
 
   void generateMusicPlaylist() {
     final playList = ConcatenatingAudioSource(
@@ -46,6 +53,8 @@ class _RunPageState extends State<RunPage> {
   void initState() {
     super.initState();
 
+    _initializeGoalSpeed();
+
     getOrGenerateUserId().then((userId) {
       //ログの生成
       speedMeterLog = speedMeterLogManager(userId, DateTime.now().toString());
@@ -59,23 +68,25 @@ class _RunPageState extends State<RunPage> {
         speedMeterLog?.getSpeed();
       });
     });
-    Timer.periodic(Duration(seconds: 10),(timer){
-      setState((){
-          if((speedMeterLog?.lowpassFilteredSpeed ?? goalSpeed) < goalSpeed -0.2){
-            playbackBpm ++;
-            player.setSpeed(playbackBpm / MusicPlaylist[0].bpm);
-          }else if((speedMeterLog?.lowpassFilteredSpeed ?? goalSpeed) > goalSpeed +0.2){
-            playbackBpm --;
-            player.setSpeed(playbackBpm / MusicPlaylist[0].bpm);
-          }
+    Timer.periodic(Duration(seconds: 10), (timer) {
+      setState(() {
+        if ((speedMeterLog?.lowpassFilteredSpeed ?? goalSpeed)! <
+            goalSpeed! - 0.2) {
+          playbackBpm++;
+          player.setSpeed(playbackBpm / MusicPlaylist[0].bpm);
+        } else if ((speedMeterLog?.lowpassFilteredSpeed ?? goalSpeed)! >
+            goalSpeed! + 0.2) {
+          playbackBpm--;
+          player.setSpeed(playbackBpm / MusicPlaylist[0].bpm);
+        }
       });
     });
   }
 
   @override
-  void dispose(){
+  void dispose() {
     player.dispose();
-}
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -125,12 +136,12 @@ class _RunPageState extends State<RunPage> {
               const SizedBox(
                 height: 20,
               ),
-              SvgPicture.asset(
-                'images/exercise.svg',
-                semanticsLabel: 'Running',
-                width: 200,
-                height: 200,
-              ),
+              // SvgPicture.asset(
+              //   'images/exercise.svg',
+              //   semanticsLabel: 'Running',
+              //   width: 200,
+              //   height: 200,
+              // ),
               const SizedBox(
                 height: 20,
               ),
@@ -141,22 +152,26 @@ class _RunPageState extends State<RunPage> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ElevatedButton(onPressed: (){
-                    setState(() {
-                      playbackBpm --;
-                      player.setSpeed(playbackBpm / MusicPlaylist[0].bpm);
-                    });
-                  },child: Icon(Icons.remove)),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          playbackBpm--;
+                          player.setSpeed(playbackBpm / MusicPlaylist[0].bpm);
+                        });
+                      },
+                      child: Icon(Icons.remove)),
                   Text(
                     '${playbackBpm.round()}',
                     style: Theme.of(context).textTheme.headlineLarge,
                   ),
-                  ElevatedButton(onPressed: (){
-                    setState(() {
-                      playbackBpm ++;
-                      player.setSpeed(playbackBpm / MusicPlaylist[0].bpm);
-                    });
-                  },child: Icon(Icons.add)),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          playbackBpm++;
+                          player.setSpeed(playbackBpm / MusicPlaylist[0].bpm);
+                        });
+                      },
+                      child: Icon(Icons.add)),
                 ],
               ),
 
@@ -169,6 +184,11 @@ class _RunPageState extends State<RunPage> {
               ),
 
               /*GPSテスト用の速度表示部分　ここまで */
+
+              // movePace と moveSpeed のデバッグ用表示
+              Text(goalSpeed != null
+                  ? '理想スピード: ${goalSpeed?.toStringAsFixed(2)}m/s (${convertSpeedToMovePace(goalSpeed!)?.toStringAsFixed(2)}min/km)'
+                  : "Speed not set"),
 
               Padding(
                 padding: const EdgeInsets.only(top: 20.0),
