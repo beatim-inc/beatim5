7800085451c40797c556ef6fca51bebda806c056
NomuKen13 <110008733+NomuKen13@users.noreply.github.com>
2023-09-19 16:19:41 +0900

    ゲインの調整

--- lib/screens/run_page.dart
+++ lib/screens/run_page.dart
@@ -33,6 +33,7 @@ class _RunPageState extends State<RunPage> {
   double? goalSpeed;
 
   double changeSpeedHurdle = 0.1;
+  double changeHighSpeedHurdle = 0.3;
   _initializeGoalSpeed() async {
     goalSpeed = await getGoalSpeed();
     setState(() {});
@@ -75,11 +76,17 @@ class _RunPageState extends State<RunPage> {
     });
     Timer.periodic(Duration(seconds: 10),(timer){
       setState((){
-          if((speedMeterLog?.lowpassFilteredSpeed ?? goalSpeed)! < goalSpeed! - changeSpeedHurdle){
-            playbackBpm ++;
+          if((speedMeterLog?.lowpassFilteredSpeed ?? goalSpeed)! < goalSpeed! - changeHighSpeedHurdle){
+            playbackBpm += 3;
+            adjustSpeed();
+          }else if((speedMeterLog?.lowpassFilteredSpeed ?? goalSpeed)! < goalSpeed! + changeSpeedHurdle){
+            playbackBpm += 1;
             adjustSpeed();
           }else if((speedMeterLog?.lowpassFilteredSpeed ?? goalSpeed)! > goalSpeed! + changeSpeedHurdle){
-            playbackBpm --;
+            playbackBpm -= 1;
+            adjustSpeed();
+          }else if((speedMeterLog?.lowpassFilteredSpeed ?? goalSpeed)! > goalSpeed! + changeHighSpeedHurdle){
+            playbackBpm -= 3;
             adjustSpeed();
           }
       });
