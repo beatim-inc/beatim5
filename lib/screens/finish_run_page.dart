e25295a11118dca1009230f4b408d8c01e03fb52
NomuKen13 <110008733+NomuKen13@users.noreply.github.com>
2023-09-20 11:55:31 +0900

    ホームに戻れるようにUIを修正

--- lib/screens/finish_run_page.dart
+++ lib/screens/finish_run_page.dart
@@ -1,3 +1,4 @@
+import 'package:beatim5/screens/welcome_page.dart';
 import 'package:beatim5/templates/base_layout.dart';
 import 'package:beatim5/widgets/page_transition_button.dart';
 import 'package:flutter/material.dart';
@@ -29,14 +30,28 @@ class FinishRunPage extends BaseLayout {
 
   @override
   Widget buttomContent(BuildContext context) {
-    return PageTransitionButton(
-      '回答する',
-      () {
-        getOrGenerateUserId().then((userId) {
-          final url = Uri.parse('https://docs.google.com/forms/d/e/1FAIpQLScvgQVnoyl9ko-K-U7SDx2eN7Pi_HQaDypuy10stb3TqpRCig/viewform?usp=pp_url&entry.530414108=$userId');
-          launchUrl(url);
-        });
-      },
+    return Column(
+      children: [
+        PageTransitionButton(
+          '回答する',
+          () {
+            getOrGenerateUserId().then((userId) {
+              final url = Uri.parse('https://docs.google.com/forms/d/e/1FAIpQLScvgQVnoyl9ko-K-U7SDx2eN7Pi_HQaDypuy10stb3TqpRCig/viewform?usp=pp_url&entry.530414108=$userId');
+              launchUrl(url);
+            });
+          },
+        ),
+        PageTransitionButton(
+            '最初の画面へ',
+            (){
+              Navigator.push<void>(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (BuildContext context) => WelcomePage(),
+                  ));
+          },
+        ),
+      ],
     );
   }
 }
