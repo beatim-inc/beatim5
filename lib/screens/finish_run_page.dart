0ffe565f72b532bd6a31f03b349a601e9fa7ff7d
NomuKen13 <110008733+NomuKen13@users.noreply.github.com>
2023-09-20 14:45:47 +0900

    レイアウトをさらに修正

--- lib/screens/finish_run_page.dart
+++ lib/screens/finish_run_page.dart
@@ -1,5 +1,4 @@
 import 'package:beatim5/screens/welcome_page.dart';
-import 'package:beatim5/templates/base_layout.dart';
 import 'package:beatim5/widgets/page_transition_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -30,82 +29,84 @@ class FinishRunPage extends StatelessWidget {
           },
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          const Padding(
-            padding: EdgeInsets.only(top: 30),
-            child: SizedBox(
-              width: 352,
-              // explanation SizedBox の Width が 83　なので 52, 135
-              height: 135,
-              child: Center(
-                child: Text(
-                  'お疲れ様でした！',
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: SizedBox(
+                width: 352,
+                // explanation SizedBox の Width が 83　なので 52, 135
+                height: 135,
+                child: Center(
+                  child: Text(
+                    'お疲れ様でした！',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(top: 14),
-            child:SizedBox(
-              width: 280,
-              height: 83,
-              child: Center(
-                child: Text(
-                  'リズムは合っていましたか？アンケートへのご協力お願いします。',
-                  style: TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.bold,
+            const Padding(
+              padding: EdgeInsets.only(top: 10),
+              child:SizedBox(
+                width: 280,
+                height: 83,
+                child: Center(
+                  child: Text(
+                    'リズムは合っていましたか？アンケートへのご協力お願いします。',
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
+              )
+            ),
+            SizedBox(height: 300, child:Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: SvgPicture.asset(
+                'images/thumbs-up.svg',
+                semanticsLabel: 'Shake Smartphone',
+                width: 200,
+                height: 200,
+              ),
+            ),),
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child:Column(
+                children: [
+                  PageTransitionButton(
+                    '回答する',
+                        () {
+                      getOrGenerateUserId().then((userId) {
+                        final url = Uri.parse('https://docs.google.com/forms/d/e/1FAIpQLScvgQVnoyl9ko-K-U7SDx2eN7Pi_HQaDypuy10stb3TqpRCig/viewform?usp=pp_url&entry.530414108=$userId');
+                        launchUrl(url);
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  PageTransitionButton(
+                    '最初の画面へ',
+                        (){
+                      Navigator.push<void>(
+                          context,
+                          MaterialPageRoute<void>(
+                            builder: (BuildContext context) => WelcomePage(),
+                          ));
+                    },
+                  ),
+                ],
               ),
             )
-          ),
-          SizedBox(height: 300, child:Padding(
-            padding: const EdgeInsets.all(40.0),
-            child: SvgPicture.asset(
-              'images/thumbs-up.svg',
-              semanticsLabel: 'Shake Smartphone',
-              width: 200,
-              height: 200,
-            ),
-          ),),
-          Padding(
-            padding: const EdgeInsets.only(top: 10.0),
-            child:Column(
-              children: [
-                PageTransitionButton(
-                  '回答する',
-                      () {
-                    getOrGenerateUserId().then((userId) {
-                      final url = Uri.parse('https://docs.google.com/forms/d/e/1FAIpQLScvgQVnoyl9ko-K-U7SDx2eN7Pi_HQaDypuy10stb3TqpRCig/viewform?usp=pp_url&entry.530414108=$userId');
-                      launchUrl(url);
-                    });
-                  },
-                ),
-                PageTransitionButton(
-                  '最初の画面へ',
-                      (){
-                    Navigator.push<void>(
-                        context,
-                        MaterialPageRoute<void>(
-                          builder: (BuildContext context) => WelcomePage(),
-                        ));
-                  },
-                ),
-              ],
-            ),
-          )
-        ],
+          ],
+        ),
       ),
-    );;
+    );
   }
 }
