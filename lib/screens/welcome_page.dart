b163d99230ec3dbbe604a27871280183a7e2579a
marufura <tara20070827@gmail.com>
2023-09-20 16:18:56 +0900

    add version info

--- lib/screens/welcome_page.dart
+++ lib/screens/welcome_page.dart
@@ -1,40 +1,89 @@
 import 'package:beatim5/screens/data_collect_agree_page.dart';
-import 'package:beatim5/screens/earphones_recommend_page.dart';
 import 'package:beatim5/templates/base_layout.dart';
 import 'package:beatim5/widgets/page_transition_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-class WelcomePage extends BaseLayout {
-  @override
-  String get title => 'Beatim Runner';
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
 
-  String get explanation => 'ベータ版';
-
-  @override
-  Widget mainContent(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(40.0),
-        child: Image.asset(
-          'images/logo.png',
-          width: 300,
-          height: 300,
-        ),
-      ),
-    );
+  Future<String> getVersionInfo() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    var text = 'Ver.${packageInfo.version}';
+    return text;
   }
 
   @override
-  Widget buttomContent(BuildContext context) {
-    return PageTransitionButton(
-      '始める',
-      () {
-        Navigator.push<void>(
-            context,
-            MaterialPageRoute<void>(
-              builder: (BuildContext context) => DataCollectAgreePage(),
-            ));
-      },
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 30),
+            child: SizedBox(
+              width: 352,
+              // explanation SizedBox の Width が 83　なので 52, 135
+              height: 52,
+              child: Center(
+                child: Text(
+                  'Beatim Runner (β版)',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          FutureBuilder(
+            future: getVersionInfo(),
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasError) {
+                  return Text('エラー: ${snapshot.error}');
+                } else {
+                  return Text(snapshot.data ?? '未取得');
+                }
+              } else {
+                return const CircularProgressIndicator();
+              }
+            },
+          ),
+          SizedBox(
+              height: 480,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: Image.asset(
+                    'images/logo.png',
+                    width: 300,
+                    height: 300,
+                  ),
+                ),
+              )),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: PageTransitionButton(
+              '始める',
+              () {
+                Navigator.push<void>(
+                    context,
+                    MaterialPageRoute<void>(
+                      builder: (BuildContext context) => DataCollectAgreePage(),
+                    ));
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
